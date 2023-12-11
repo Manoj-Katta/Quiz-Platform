f7dbbf562bfad70d0ef59333468da2204d35a690
Manoj-Katta <manojkatta1173@gmail.com>
2023-12-11 19:52:15 +0530

    added articles

--- client/src/pages/Articles.jsx
+++ client/src/pages/Articles.jsx
@@ -1,24 +1,32 @@
-// Articles.jsx
-
 import React, { useEffect, useState } from 'react';
-import * as tf from '@tensorflow/tfjs';
 
 const Articles = () => {
   const [prediction, setPrediction] = useState(null);
+  const [matchingRows, setMatchingRows] = useState([]);
+  const [allRows, setAllRows] = useState([]); // State to store all rows
+  const skillToMatch = 'Javascript'; // Replace with the actual skill you want to match
 
   const loadModel = async () => {
     try {
-    const model = await tf.loadLayersModel("/models/Ascend/Javascript_recommender/model.json");
+      // Mock prediction value as 4
+      const mockPrediction = 4.02;
+      setPrediction(mockPrediction);
+  
+      // Make a request to the backend API to get matching rows
+      const response = await fetch(`http://localhost:3001/api/getRows/${mockPrediction}/${skillToMatch}`);
+      const data = await response.json();
+  
+      console.log('Matching rows from API:', data);
+      setMatchingRows(data);
 
-      const inputTensor = tf.tensor3d([[[10.02, 117.04, 3.66, 3.26, 2019, 1, 0, 0]]]);
-
-      const result = model.predict(inputTensor);
-      const predictionData = result.arraySync();
-      setPrediction(predictionData);
+      // Update allRows state to include the new rows
+      setAllRows((prevRows) => [...prevRows, ...data]);
+      console.log(allRows);
     } catch (error) {
       console.error('Error loading or predicting with the model:', error);
     }
   };
+  
 
   useEffect(() => {
     loadModel();
@@ -30,10 +38,48 @@ const Articles = () => {
       <p>Content of your Articles page goes here.</p>
 
       {/* Render prediction directly within the Articles component */}
-      {prediction && (
+      {prediction !== null && (
         <div>
           <h3>Predicted Data:</h3>
           <pre>{JSON.stringify(prediction, null, 2)}</pre>
+        </div>
+      )}
+
+      {/* Render matching rows */}
+      {matchingRows.length > 0 && (
+        <div>
+          <h3>Matching Rows from Database:</h3>
+          <ul>
+            {/* Limit to the first 3 rows */}
+            {matchingRows.slice(0, 3).map((row) => (
+              <li key={row.id}>
+                {/* Render your row data here */}
+                <p>ID: {row.id}</p>
+                <p>Title: {row.title}</p>
+                <p>Url: {row.url}</p>
+                {/* Add additional fields as needed */}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+
+      {/* Render all rows */}
+
+      {allRows.length > 0 && (
+        <div>
+          <h3>All Rows:</h3>
+          <ul>
+            {allRows.map((row) => (
+              <li key={row.id}>
+                {/* Render your row data here */}
+                <p>ID: {row.id}</p>
+                <p>Title: {row.title}</p>
+                <p>Url: {row.url}</p>
+                {/* Add additional fields as needed */}
+              </li>
+            ))}
+          </ul>
         </div>
       )}
     </div>
--- server/server.js
+++ server/server.js
@@ -1,53 +1,94 @@
+// server.js (or index.js for your backend)
+
 import express from 'express';
-import { ApolloServer } from 'apollo-server-express'; // import ApolloServer for GraphQL
+import { ApolloServer } from 'apollo-server-express';
 import * as dotenv from 'dotenv';
 import { MongoClient } from 'mongodb';
-import path from 'path'; 
-import connectDB from './config/connection.js'; // import connection to MongoDB
-import { typeDefs, resolvers } from './schemas/index.js'; // import typeDefs and resolvers
-import { authMiddleware } from './utils/auth.js'; // import authMiddleware for authentication
+import path from 'path';
+import connectDB from './config/connection.js';
+import { typeDefs, resolvers } from './schemas/index.js';
+import { authMiddleware } from './utils/auth.js';
 import cors from 'cors';
-import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
-dotenv.config(); // Load environment variables from .env file
+import { join, dirname } from 'path';
+
+dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 const mongoUrl = 'mongodb+srv://2021ucs0106:9x6hM0egCSi4t5Qf@cluster0.hm3fpk3.mongodb.net/Roadmaps?retryWrites=true&w=majority';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-// Create a new instance of an Apollo server with the GraphQL schema
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware, // This ensures that every request performs an authentication check, and the updated request object will be passed to the resolvers as the context.
+  context: authMiddleware,
 });
 
-const app = express(); // Create a new instance of an Express server
-const modelsPath = join(__dirname, '..', 'models');
-app.use('/models', express.static(modelsPath));
+const app = express();
 
-app.use(express.urlencoded({ extended: true })); // This sets up middleware to parse incoming requests with urlencoded payloads
-app.use(express.json()); // This sets up middleware to parse incoming requests with JSON payloads
+// Enable CORS
+app.use(cors());
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/api/getRows/:number/:skill', async (req, res) => {
+  const { number, skill } = req.params;
+
+  try {
+    console.log('Received request for number:', number, 'and skill:', skill);
+
+    const client = new MongoClient(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+    await client.connect();
+    console.log('Connected to MongoDB');
+
+    const database = client.db('Articles');
+    const collection = database.collection('Javascript');
+
+    // Convert number to float
+    const numericValue = parseFloat(number);
+
+    // Define a range for finding rows near the specified number
+    const range = 0.5; // Adjust this value based on your requirements
+
+    // Adjust the field names in the query based on your data structure
+    const result = await collection.find({
+      "Average Rating": { $gte: numericValue - range, $lte: numericValue + range },
+      domain: skill
+    }).limit(3).toArray();
+
+    console.log('Matching rows:', result);
+    res.json(result);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  } 
+});
+
+
+// Serve static files in production
 if (process.env.NODE_ENV === 'production') {
-app.use(express.static(path.join(new URL('../client/dist', import.meta.url).pathname)));
+  app.use(express.static(path.join(new URL('../client/dist', import.meta.url).pathname)));
 }
 
+// Send index.html for any other requests
 app.get('*', (req, res) => {
   res.sendFile(path.join(new URL('../client/dist/index.html', import.meta.url).pathname));
 });
 
-
-const startServer = async (typeDefs, resolvers) => {
+const startServer = async () => {
   try {
-    await server.start(); // Start the Apollo server
-    server.applyMiddleware({ app }); // integrate our Apollo server with the Express application as middleware
-    connectDB(process.env.MONGODB_URI); // connect to MongoDB
+    await server.start();
+    server.applyMiddleware({ app });
+    connectDB(process.env.MONGODB_URI);
+
     const client = await MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('Connected to MongoDB');
 
     const db = client.db('Roadmaps');
-    const collection = db.collection('Javascript')
+    const collection = db.collection('Javascript');
+
     app.get('/get-data', async (req, res) => {
       try {
         const data = await collection.find({}).toArray();
@@ -57,6 +98,7 @@ const startServer = async (typeDefs, resolvers) => {
         res.status(500).json({ error: 'Internal Server Error' });
       }
     });
+
     process.on('SIGINT', () => {
       client.close();
       process.exit();
@@ -71,4 +113,4 @@ const startServer = async (typeDefs, resolvers) => {
   }
 };
 
-startServer(typeDefs, resolvers); // Start server
+startServer();
